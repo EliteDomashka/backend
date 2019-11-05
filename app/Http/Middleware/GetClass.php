a4b2bd85eae3a48b0615c055a5bcee1f56d03886
Alexey <oleksih@gmail.com>
2019-11-05 16:36:05 +0200

    upd class_id get

--- app/Http/Middleware/GetClass.php
+++ app/Http/Middleware/GetClass.php
@@ -15,18 +15,28 @@ class GetClass
      * @return mixed
      */
     public function handle($request, Closure $next) {
-        $host = $request->header('origin') ?? $request->header('referer');
-        preg_match('/:\/\/((.+).domashka.cloud).*/', $host, $exp_host);
-        if($host == "http://localhost:8002") $exp_host = [2=> 'localhost'];
-        if(isset($exp_host[2])){
-            $request->attributes->add([
-                'class_domain' => $exp_host[2],
-                'class' => $class = ClassM::getByDomain($exp_host[2]),
-                'class_id' => $class_id = isset($class['id']) ? $class['id'] : null
-            ]);
-            
-            if($class_id !== null) return $next($request);
-        }
-        return response()->json(['message' => "{$host} not found"])->setStatusCode(403);
+    	if($request->get('class_id') == null){
+			$host = $request->header('origin') ?? $request->header('referer');
+			preg_match('/:\/\/((.+).domashka.cloud).*/', $host, $exp_host);
+			if($host == "http://localhost:8002") $exp_host = [2=> '13'];
+			if(isset($exp_host[2])){
+				$request->attributes->add([
+					'class_domain' => $exp_host[2],
+					'class' => $class = ClassM::getByDomain($exp_host[2]),
+					'class_id' => $class_id = isset($class['id']) ? $class['id'] : null
+				]);
+
+				if($class !== null) return $next($request);
+				return response()->json(['message' => "{$host} not found"])->setStatusCode(403);
+			}
+		}else{
+			$request->attributes->add([
+				'class' => $class = ClassM::find($request->get('class_id')),
+				'class_id' => $class_id = isset($class['id']) ? $class['id'] : null
+			]);
+
+			if($class !== null) return $next($request);
+		}
+
     }
 }
